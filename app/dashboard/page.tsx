01ba97f6b6d6f74e00bc971f1a7b3883de9b8007
Vano Qochladze <vano.qochladze@ge.ey.com>
2024-08-28 21:39:31 +0400

    card data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,15 +5,13 @@ import { lusitana } from '@/app/ui/fonts';
 import { 
     fetchRevenue,
     fetchCardData,
-    fetchLatestInvoices,
-    fetchFilteredInvoices,
-    fetchInvoicesPages,
-    fetchInvoiceById
+    fetchLatestInvoices
 } from '../lib/data';
  
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
+    const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData()
     
   return (
     <main>
@@ -21,14 +19,14 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+        { <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        /> }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue}  />
